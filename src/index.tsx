9da448b7fe3203bf2a26a035d3b4a6e4d95eee8a
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-09-01 15:05:50 +0200

    feat: create assets folder, move styles to it and add twitter icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/global.scss";
+import "./assets/styles/global.scss";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
