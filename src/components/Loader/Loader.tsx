5887c6ac313464584a8a072cb97a40278b8be89e
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-09-01 13:54:43 +0200

    fix: loader styling

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,9 @@
 import styles from "./Loader.module.scss";
 
 export const Loader = () => {
-  return <div className={styles.loader}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}></div>
+    </div>
+  );
 };
