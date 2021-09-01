e8b11101be32074a593516c2dd46410e0f3edb28
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-09-01 15:37:13 +0200

    fix: text in loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,6 +3,7 @@ import styles from "./Loader.module.scss";
 export const Loader = () => {
   return (
     <div className={styles.wrapper}>
+      <span aria-label="loading..."></span>
       <div className={styles.loader}></div>
     </div>
   );
