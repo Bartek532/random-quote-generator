3a9ea92d0b00c177fcce1b418b92448c149ea985
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-09-01 13:51:54 +0200

    fix: improve useRandomQuote hook

--- src/utils/hooks/useRandomQuote.tsx
+++ src/utils/hooks/useRandomQuote.tsx
@@ -1,25 +1,36 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+type Quote = {
+  _id: string;
+  content: string;
+  author: string;
+  authorSlug: string;
+  length: number;
+  tags: string[];
+};
+
 export const useRandomQuote = () => {
-  const [quote, setQuote] = useState("");
+  const [quote, setQuote] = useState({} as Quote);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchQuote = async () => {
-      setLoading(true);
-      try {
-        const { data } = await axios.get("https://api.quotable.io/random");
-        setQuote(data);
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchNextQuote = async () => {
+    setLoading(true);
+    try {
+      const { data }: { data: Quote } = await axios.get(
+        "https://api.quotable.io/random"
+      );
+      setQuote(data);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchQuote();
+  useEffect(() => {
+    fetchNextQuote();
   }, []);
 
-  return { quote, loading };
+  return { quote, loading, fetchNextQuote };
 };
